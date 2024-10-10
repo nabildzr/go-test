a780249a61d8aaf6b2734129c82c40e616a5296a
nabildzr <muhammadnbillz@gmail.com>
2024-10-10 20:21:22 +0700

    add

---
+++ che.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(len("Hello World"))
+
+
+	fmt.Print("Hello"[0])
+	fmt.Print("Hello Bill"[1])
+}
---
+++ va.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	
+	name = "Nabil Dzikrika"
+	fmt.Println(name)
+}
